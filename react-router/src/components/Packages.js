294b429073ecf1b0a574b1eb7b0676bc078e7004
unknown <kylelarsen12@gmail.com>
2022-08-06 12:29:40 -0400

    Complete activity

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,15 +1,15 @@
-
-
-export default function Packages() {
-
-    return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                
-            </div>
+export default function Packages(props) {
+  const displayPackages = props.packages.map((eachPackage) => (
+    <li>{eachPackage}</li>
+  ));
+  return (
+    <div>
+      <div className="packages">
+        <div className="packagesHeader">
+          <h1>Our Packages</h1>
         </div>
-    )
+        <ul>{displayPackages}</ul>
+      </div>
+    </div>
+  );
 }
