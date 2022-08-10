a3858e72e874fe7d4bd5a75a8ef56b46f953553f
unknown <kylelarsen12@gmail.com>
2022-08-10 18:51:11 -0400

    Add bootstrap styling to project

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,15 +1,23 @@
+import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+
 export default function Packages(props) {
   const displayPackages = props.packages.map((eachPackage) => (
-    <li>{eachPackage}</li>
+    <ListGroup.Item>{eachPackage}</ListGroup.Item>
   ));
   return (
-    <div>
-      <div className="packages">
-        <div className="packagesHeader">
-          <h1>Our Packages</h1>
-        </div>
-        <ul>{displayPackages}</ul>
-      </div>
-    </div>
+    <Container>
+      <Card>
+        <Card.Body>
+          <Card.Title>Our Packages</Card.Title>
+          <Card.Text>
+            Check out some of our packages! Every package is ethically sourced
+            and organic!
+          </Card.Text>
+        </Card.Body>
+        <ListGroup>{displayPackages}</ListGroup>
+      </Card>
+    </Container>
   );
 }
